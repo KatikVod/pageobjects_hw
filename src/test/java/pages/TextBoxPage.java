f4958a7f854b736103e82a15e3e40c4489c85285
Ekaterina Vodolazhskaia <katik_v@inbox.ru>
2024-05-03 17:55:49 +0300

    added PracticeFormTests and TextBoxTests

---
+++ src/test/java/pages/TextBoxPage.java
@@ -0,0 +1,62 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.OutputComponent;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class TextBoxPage {
+
+    private final SelenideElement userNameInput = $("#userName"),
+            userEmailInput = $("#userEmail"),
+            currentAddressInput = $("#currentAddress"),
+            permanentAddressInput = $("#permanentAddress"),
+            submitButton = $("#submit");
+
+    OutputComponent outputComponent = new OutputComponent();
+
+    public TextBoxPage openPage() {
+        open("/text-box");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+    public TextBoxPage setUserName(String value) {
+        userNameInput.setValue(value);
+
+        return this;
+    }
+
+    public TextBoxPage setUserEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public TextBoxPage setCurrentAddress(String value) {
+        currentAddressInput.setValue(value);
+
+        return this;
+    }
+
+    public TextBoxPage setPermanentAddress(String value) {
+        permanentAddressInput.setValue(value);
+
+        return this;
+    }
+
+    public TextBoxPage submit() {
+        submitButton.click();
+
+        return this;
+    }
+
+    public TextBoxPage checkResult(String key, String value) {
+        outputComponent.checkResult(key, value);
+
+        return this;
+    }
+}
